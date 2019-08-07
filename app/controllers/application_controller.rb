e75876cf870fa8af848dddf602af2c837e292ab3
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-08-07 11:42:32 +0100

    Sinatra RESTful Routes Complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -6,4 +7,52 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  #index
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb:index
+  end
+
+  #new
+  get '/recipes/new' do
+    erb :new
+  end
+  
+  #create
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  #show
+  get '/recipes/:id' do
+    # binding.pry
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  #edit
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+
+  erb :edit
+  end
+
+  #update
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  #destroy
+  delete '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+    redirect '/recipes'
+  end
 end
